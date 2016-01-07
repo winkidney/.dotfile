38b1ea0781c4adab98ff40329f9e35b7b0b9e8ab
winkidney <winkidney@gmail.com>
2016-01-07 06:22:37 -0800

    Add ubuntu software install script

---
+++ scripts/ub_install.sh
@@ -0,0 +1,9 @@
+# install
+sudo apt-get install python-pip
+sudo apt-get install zsh
+sudo apt-get install terminator
+sudo apt-get install vim
+
+# remove
+sudo apt-get auto-remove firefox
+sudo apt-get auto-remove libre-office*
