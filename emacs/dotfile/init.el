c99f09837e66b2f61429a844b80af27810deeadd
winkidney <winkidney@gmail.com>
2017-01-29 17:34:16 +0800

    Update: Add emacs config and related tmux config

---
+++ emacs/dotfile/init.el
@@ -0,0 +1,33 @@
+;; init.el --- Emacs configuration
+
+;; INSTALL PACKAGES
+;; --------------------------------------
+
+(require 'package)
+
+(add-to-list 'package-archives
+       '("melpa" . "http://elpa.emacs-china.org/melpa/") t)
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar kidney_package
+  '(better-defaults
+    elpy
+    material-theme))
+
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      kidney_package)
+
+;; BASIC CUSTOMIZATION
+;; --------------------------------------
+
+;; (setq inhibit-startup-message t) ;; hide the startup message
+(load-theme 'material t) ;; load material theme
+(global-linum-mode t) ;; enable line numbers globally
+(elpy-enable)
+(elpy-use-ipython)
+;; init.el ends here
