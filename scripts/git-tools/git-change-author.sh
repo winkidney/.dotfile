7cacc19e6d37e908cd546f1f56bfd2ed6eb3d456
Ji Qu <winkidney@gmail.com>
2018-08-19 21:06:48 +0800

    Fix: Fix bug for git rename-author script

--- scripts/git-tools/git-change-author.sh
+++ scripts/git-tools/git-change-author.sh
@@ -1,19 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 git filter-branch --env-filter '
+WRONG_EMAIL="${1}"
+NEW_NAME="${2}"
+NEW_EMAIL="${3}"
 
-OLD_EMAIL=$1
-CORRECT_NAME=$2
-CORRECT_EMAIL=$3
-
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
 then
-    export GIT_COMMITTER_NAME="$CORRECT_NAME"
-    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
 then
-    export GIT_AUTHOR_NAME="$CORRECT_NAME"
-    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
 fi
 ' --tag-name-filter cat -- --branches --tags
